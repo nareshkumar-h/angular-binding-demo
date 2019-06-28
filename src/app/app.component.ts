314d55908b1da63df338dc82a42c7c33afddf535
Naresh Kumar H <nareshkumarh@live.com>
2019-06-28 16:28:09 -0400

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding-demo';
+  title = 'data-binding-demo1';
+  username:string ;
+
+  myFunction(){
+    alert("Button clicked");
+  }
 }
